ff9d83f779322721876254963a88a587e2d9be64
Acer <Acer@DESKTOP-GB9TLRK>
2023-10-20 21:36:03 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicThreadingLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart threadstart = new ThreadStart(MyThreadClass.Thread1);
+
+            //Create two threads named "ThreadA" and "ThreadB"
+            Thread ThreadA = new Thread(threadstart);
+            ThreadA.Name = "Thread A Process";
+            Thread ThreadB = new Thread(threadstart);
+            ThreadB.Name = "Thread B Process";
+            Console.WriteLine(label1.Text);
+
+            //Starting a thread
+            ThreadA.Start();
+            ThreadB.Start();
+
+            //Use the join() method to stop calling the thread until the thread terminates
+            ThreadA.Join();
+            ThreadB.Join();
+
+            label1.Text = "---End of Thread---";
+            Console.WriteLine(label1.Text);
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThreadingLab
+{
+    internal class MyThreadClass
+    {
+        //declaring a static method named Thread1
+        public static void Thread1()
+        {
+            //create a loop that terminates the thread in the fifth attempt
+            for (int i = 0; i <= 5; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + i);
+
+                //using sleep() method to suspend a thread
+                Thread.Sleep(1500);
+            }
+
+        }
+    }
+}
